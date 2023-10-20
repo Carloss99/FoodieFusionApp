6ec02b8543f7e431e9f84bc3ed519ccf6d6bbd26
dahee-lee <dahlee97@gmail.com>
2023-10-20 10:29:20 -0400

    added userReviewsLogin page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+
 const Header = (props) => {
     return(
-        <h1 className='header'>Foodie Fusion</h1>
+        <div className="header">
+            <nav>
+                <Link to="/">
+                    <div>Home</div>
+                </Link>
+                <Link to="/reviews">
+                    <div>Reviews</div>
+                </Link>
+                <Link to="/userreviews">
+                    <div>Profile</div>
+                </Link>
+            </nav>
+        </div>
     )
 }
 
---
+++ src/pages/UserReviewsLogin.js
@@ -0,0 +1,153 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const UserReviewsLogin = () => {
+    // create state variables
+    const [toggleLogin, setToggleLogin] = useState(true)
+    const [toggleError, setToggleError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const [toggleLogout, setToggleLogout] = useState(false)
+    const [currentUser, setCurrentUser] = useState({})
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    // function for the toggleError state variable
+    // if toggleLogin is true aka user is logged in, the login form will not show
+    // else, the login form will show to allow user to log in
+    // div name is formContainer
+    const handleToggleForm = () => {
+        setToggleError(false)
+        if (toggleLogin === true) {
+            setToggleLogin(false)
+        } else {
+            setToggleLogin(true)
+        }
+    }
+
+    //this function is used w the event handler on the logout button
+    //if user is logged in, toggleLogout = true
+    //if user is logged out, toggleLogout = false
+    const handleLogout = () => {
+        setCurrentUser({})
+        handleToggleLogout()
+    }
+    //once user presses log out, handleToggleLogout() sets toggleLgout to false
+    const handleToggleLogout = () => {
+        if (toggleLogout) {
+            setToggleLogout(false)
+        } else {
+            setToggleLogout(true)
+        }
+    }
+
+    const handleCreateUser = (event) => {
+        event.preventDefault()
+        event.currentTarget.reset()
+        let userObj = {
+          username: username,
+          password: password
+        }
+        setUsername('')
+        setPassword('')
+        axios.post('http://localhost:4000/api/userreviews/createaccount', userObj).then((response) => {
+          if(response.data.username){
+            console.log(response);
+            setToggleError(false)
+            setErrorMessage('')
+            setCurrentUser(response.data)
+            handleToggleLogout()
+          } else {
+            setErrorMessage(response.data)
+            setToggleError(true)
+          }
+        })
+      }
+      
+    //this functions takes the onClick event listener. It sets the currentUser state variable to ...
+    const handleLogin = (event) => {
+        event.preventDefault()
+        event.currentTarget.reset()
+        let userObj = {
+            username: username,
+            password: password
+        }
+        setUsername('')
+        setPassword('')
+        axios.put('http://localhost:4000/api/userreviews/login', userObj).then((response) => {
+            if (response.data.username) {
+                console.log(response);
+                setToggleError(false)
+                setErrorMessage('')
+                setCurrentUser(response.data)
+                handleToggleLogout()
+            } else {
+                console.log(response);
+                setToggleError(true)
+                setErrorMessage(response.data)
+            }
+        })
+    }
+
+    return (
+        <div className="userReviewDiv">
+
+            {/* if user is logged in, show the logout button */}
+            {toggleLogout ?
+                <button onClick={handleLogout} class='logoutBtn'>Logout</button> :
+
+                //else, if user is logged out, show this div with login form or registration form
+                <div class='appFormDiv'>
+
+                    {/* if toggleLogin is true, aka user is not logged in, show the login form div */}
+                    {toggleLogin ?
+                        <div className="formContainer">
+                            <h1 class='formTitle'>Login</h1>
+                            {/* when user presses login, it invokes handleLogin() */}
+                            <form onSubmit={handleLogin} class='inputForm'>
+                                <input type='text' placeholder='username' class='textInput' onChange={(event) => { setUsername(event.target.value) }} />
+                                <input type='password' placeholder='password' class='textInput' onChange={(event) => { setPassword(event.target.value) }} />
+                                {toggleError ?
+                                    <h5 class='errorMsg'>{errorMessage}</h5>
+                                    :
+                                    null
+                                }
+                                <input type='submit' value='Login' class='submitBtn' />
+                            </form>
+                        </div>
+                        :
+                        // new user registration form
+                        // the button below "need an account" will show with the login form. It invokes handleToggleForm() which changes toggleLogin to false
+                        <div className="App" class='formContainer'>
+                            <h1 class='formTitle'>Create an Account</h1>
+                            <form onSubmit={handleCreateUser} class='inputForm'>
+                                <input type='text' placeholder='username' class='textInput' onChange={(event) => { setUsername(event.target.value) }} />
+                                <input type='password' placeholder='password' class='textInput' onChange={(event) => { setPassword(event.target.value) }} />
+                                {toggleError ?
+                                    <h5 class='errorMsg'>{errorMessage}</h5>
+                                    :
+                                    null
+                                }
+                                <input type='submit' value='Register' class='submitBtn' />
+                            </form>
+                        </div>
+                    }
+                    <button onClick={handleToggleForm} class='accountBtn'>{toggleLogin ? 'Need an account?' : 'Already have an account?'}</button>
+                </div>
+            }
+
+
+            {/* div with user profile info and reviews shown only if user is logged in */}
+            {currentUser.username ?
+                <div className="loggedinDiv">
+                    <h2>User Reviews Shown Once Logged In</h2>
+                    <h4>Profile Info</h4>
+                </div>
+                : null
+            }
+
+        </div>
+    )
+}
+
+export default UserReviewsLogin
